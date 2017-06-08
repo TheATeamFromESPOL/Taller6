5836ce981b6f7cabbe76921ed03454871e5b1ab6
llullanqui <luis.1142@hotmail.com>
2017-06-08 16:04:58 -0500

    kjahda

--- taller6.c
+++ taller6.c
@@ -4,11 +4,16 @@ void desglosar(char *fecha);
 void intercambiar(int a, int b);
 int direccion(int *arreglo[]);
 
-int main(){
+int main(void){
+	char *fecha= "2017-06-06";
+	desglosar(fecha);
 }
 
 void desglosar(char *fecha){
-	
+	printf("Día: %.*s\n",2,fecha+8);
+	printf("Mes: %.*s\n",2,fecha+5);
+	printf("Año: %.*s\n",4,fecha+0);
+
 }
 
 void intercambiar(int a, int b){
@@ -16,5 +21,5 @@ void intercambiar(int a, int b){
 }
 
 int direccion(int *arreglo[]){
-	
+	return 0;
 }
