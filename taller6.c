69bea448a27b134c51406e22ad138d02f8051b0c
llullanqui <luis.1142@hotmail.com>
2017-06-08 15:50:42 -0500

    kajshda

---
+++ taller6.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void desglosar(char *fecha);
+void intercambiar(int a, int b);
+int direccion(int *arreglo[]);
+
+int main(){
+}
+
+void desglosar(char *fecha){
+	
+}
+
+void intercambiar(int a, int b){
+	
+}
+
+int direccion(int *arreglo[]){
+	
+}
